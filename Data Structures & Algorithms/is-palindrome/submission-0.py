5e80225e12d7b8166a302dd623cee8443b971827
Sara <sarachlihi4@gmail.com>
2026-07-01 14:12:52 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.replace(" ","")
+        l = 0
+        r = len(s) -1
+        while l < r:
+
+            if s[l].isalnum():
+                if s[r].isalnum():
+                    if s[l].lower() == s[r].lower():
+                        l += 1 
+                        r -= 1
+                    else:
+                        return False
+                else:
+                    r -= 1
+                
+            else:
+                l += 1
+        return True
+          
+           
+
+
+
+       
+            
